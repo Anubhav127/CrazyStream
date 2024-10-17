c9d6d62a6494a1824376b7ea44951a70d8afd935
Anubhav127 <anu.verma712@gmail.com>
2024-10-18 04:21:18 +0530

    just checking

--- Frontend/src/components/Authentication/Login.jsx
+++ Frontend/src/components/Authentication/Login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
     e.preventDefault();
     try {
 
-      const response = await axios.post("http://localhost:3000/users/login", data, {
+      const response = await axios.post("http://192.168.253.36:3000/users/login", data, {
         headers: {
           'Content-Type': 'application/json'
         }
