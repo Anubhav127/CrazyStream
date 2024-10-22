7a59d26ade8e53255804bf63432a5142ce6a3c14
Anubhav Verma <147929948+Anubhav127@users.noreply.github.com>
2024-10-22 08:55:38 +0530

    Update Register.jsx

--- Frontend/src/components/Authentication/Register.jsx
+++ Frontend/src/components/Authentication/Register.jsx
@@ -39,7 +39,7 @@ const Register = () => {
       formData.append('avatar', image);
       console.log(formData);
       
-      const response = await axios.post('https://192.168.253.36:8000/users/register', formData, {
+      const response = await axios.post('https://virtualhosting.onrender.com/users/register', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
